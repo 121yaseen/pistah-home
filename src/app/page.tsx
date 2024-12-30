6b86fb4d195af2ebea986af3e00ec9d23295bcf1
Yaseen <121yaseen@gmail.com>
2024-12-30 15:08:11 +0530

    make page GOLD

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,17 @@
+"use client";
+
 export default function Home() {
   return (
     <div>
-      <h1 style={{ fontSize: "600px", textAlign: "center" }}>Pistah</h1>
+      <h1
+        className="logo"
+        style={{
+          fontSize: "600px",
+          textAlign: "center",
+        }}
+      >
+        Pistah
+      </h1>
     </div>
   );
 }
