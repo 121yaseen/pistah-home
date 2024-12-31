368b8d01a44ceb722c736fb80e5d9fee9cbeaea2
Josen Jose <josen.jose@soti.net>
2024-12-31 17:26:14 +0530

    added header

---
+++ src/app/components/HeaderComponent.tsx
@@ -0,0 +1,55 @@
+import PistahIcon from "./icons/pistahIcon";
+
+const HeaderComponent = () => {
+  return (
+    <div
+      style={{
+        position: "fixed",
+        top: 0,
+        left: 0,
+        width: "100%",
+        height: "100px",
+        backgroundColor: "transparent",
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        padding: "0 60px",
+        zIndex: 1000,
+      }}
+    >
+      <a href="/">
+        <PistahIcon />
+      </a>
+
+      {/* Right Button: Contact Us */}
+      <button
+        style={{
+          position: "relative",
+          right: "50px",
+          top: "8px",
+          padding: "10px 20px",
+          backgroundColor: "transparent",
+          color: "white",
+          border: "none",
+          fontWeight: "bold",
+          fontSize: "14px",
+          cursor: "pointer",
+          transition: "all 0.2s ease",
+          textDecoration: "none",
+        }}
+        onMouseEnter={(e) => {
+          const target = e.target as HTMLElement;
+          target.style.color = "#0b57d0";
+        }}
+        onMouseLeave={(e) => {
+          const target = e.target as HTMLElement;
+          target.style.color = "white";
+        }}
+      >
+        Contact Us
+      </button>
+    </div>
+  );
+};
+
+export default HeaderComponent;
--- src/app/components/HomeScreenComponent.tsx
+++ src/app/components/HomeScreenComponent.tsx
@@ -1,10 +1,15 @@
+import HeaderComponent from "./HeaderComponent";
 import HeroComponent from "./HeroComponent";
 import DSPFeaturesSectionComponent from "./DSPFeaturesSectionComponent";
 import SSPFeaturesSectionComponent from "./SSPFeaturesSectionComponent";
 
 const HomeScreenComponent = () => {
   return (
-    <div className="h-screen overflow-y-scroll snap-y snap-mandatory">
+    <div className="h-screen overflow-y-scroll snap-y snap-mandatory relative">
+      {/* Fixed Header */}
+      <HeaderComponent />
+
+      {/* Main Content */}
       <section className="h-screen snap-start">
         <HeroComponent />
       </section>
