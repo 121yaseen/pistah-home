8e93034fa98ba6cd1ce73c844e6fe7621406a4cc
Josen Jose <josen.jose@soti.net>
2024-12-31 21:40:27 +0530

    moved header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 "use client";
 
+import HeaderComponent from "./components/HeaderComponent";
 import HomeScreenComponent from "./components/HomeScreenComponent";
 
 export default function Home() {
   return (
     <div>
+      <HeaderComponent />
       <HomeScreenComponent />
     </div>
   );
