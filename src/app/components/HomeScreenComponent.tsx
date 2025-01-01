d5379d3a8a7159944903456a229eb67a81841a7c
JZN-37 <89829197+JZN-37@users.noreply.github.com>
2025-01-01 09:29:17 +0530

    changes hero (#11)
    * effects
    
    * moved header
    
    * changed to link
    
    * added animation
    
    * moved to assets
    
    * hero mobile changes
    
    ---------
    
    Co-authored-by: Josen Jose <josen.jose@soti.net>

--- src/app/components/HomeScreenComponent.tsx
+++ src/app/components/HomeScreenComponent.tsx
@@ -1,4 +1,3 @@
-import HeaderComponent from "./HeaderComponent";
 import HeroComponent from "./HeroComponent";
 import DSPFeaturesSectionComponent from "./DSPFeaturesSectionComponent";
 import SSPFeaturesSectionComponent from "./SSPFeaturesSectionComponent";
@@ -7,9 +6,6 @@ import FooterComponent from "./FooterComponent";
 const HomeScreenComponent = () => {
   return (
     <div className="h-screen overflow-y-scroll snap-y snap-mandatory relative">
-      {/* Fixed Header */}
-      <HeaderComponent />
-
       {/* Main Content */}
       <section className="h-screen snap-start">
         <HeroComponent />
