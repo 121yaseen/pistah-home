a1613aa7993db88318f80e51feb02f757ab0df1b
JZN-37 <89829197+JZN-37@users.noreply.github.com>
2025-01-12 23:31:55 +0530

    all changes (#23)
    * all changes
    
    * build fix
    
    ---------
    
    Co-authored-by: Josen Jose <josen.jose@soti.net>

---
+++ src/app/demo/page.tsx
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <div>
+      <iframe
+        src="https://calendar.google.com/calendar/appointments/schedules/AcZssZ2zWAwMWNElL2HYdqErY-hjbXHZTBMEx11SXrVbfH7hrMLuGGaWSDdIGyYSbpUyYNRviKgNQznG?gv=true"
+        style={{
+          width: "100%",
+          height: "100vh",
+          backgroundColor: "white",
+        }}
+      />
+    </div>
+  );
+}
