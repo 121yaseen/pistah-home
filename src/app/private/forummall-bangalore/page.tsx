1a7aec73c3703a240a0d62b5f85b9133ba0cdec7
JZN-37 <89829197+JZN-37@users.noreply.github.com>
2025-01-24 10:49:08 +0530

    forum mall (#24)
    Co-authored-by: Josen Jose <josen.jose@soti.net>

---
+++ src/app/private/forummall-bangalore/page.tsx
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <div>
+      <iframe
+        src="https://docs.google.com/presentation/d/e/2PACX-1vSdbDIioHVTccfu4jq0YWQuIBOZOISNyOZBTnlFUark2SuyoxRJG2jvpMRRaVpqDap-5nrDMw2CHaSt/embed?start=true&loop=false&delayms=60000"
+        style={{
+          width: "100%",
+          height: "105vh",
+          border: "none",
+        }}
+        allowFullScreen={true}
+      ></iframe>
+    </div>
+  );
+}
