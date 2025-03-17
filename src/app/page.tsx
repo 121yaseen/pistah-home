1128cdf3de8932a9ea2717fa721aa5164931b7fe
JZN-37 <89829197+JZN-37@users.noreply.github.com>
2025-03-17 13:11:33 +0530

    text change (#36)
    Co-authored-by: Josen Jose <josen.jose@soti.net>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 "use client";
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import HeaderComponent from "./components/HeaderComponent";
 import HomeScreenComponent from "./components/HomeScreenComponent";
 
 export default function Home() {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
   const [isHeaderVisible, setIsHeaderVisible] = useState(true);
 
   const handleScrollDirection = (direction: 'up' | 'down') => {
